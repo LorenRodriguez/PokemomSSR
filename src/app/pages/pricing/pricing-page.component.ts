d432e1a1173e36ebc9b5d73156e219088dbd6ef9
LorenRodriguez <loren.rr@hotmail.com>
2025-06-20 16:33:48 +0200

    feat: add server-side rendering support and improve SEO for pages
    - Updated package.json to include SSR dependencies and scripts.
    - Configured app.config.ts for client hydration.
    - Enhanced About, Contact, and Pricing page components with dynamic title and meta tags.
    - Fixed navigation links in the navbar component.
    - Added server configuration in app.config.server.ts for SSR.
    - Defined server routes in app.routes.server.ts for prerendering.
    - Implemented server bootstrap logic in main.server.ts.
    - Created server.ts to handle requests and serve the Angular application.

--- src/app/pages/pricing/pricing-page.component.ts
+++ src/app/pages/pricing/pricing-page.component.ts
@@ -1,4 +1,12 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { isPlatformBrowser, isPlatformServer } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  OnInit,
+  PLATFORM_ID,
+} from '@angular/core';
+import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'pricing-page',
@@ -6,4 +14,21 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   templateUrl: './pricing-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class PricingPageComponent {}
+export default class PricingPageComponent implements OnInit {
+  private title = inject(Title);
+  private meta = inject(Meta);
+  private platform = inject(PLATFORM_ID);
+
+  ngOnInit(): void {
+    // document.title = 'Prueba';
+    //if (isPlatformBrowser(this.platform)) document.title = 'Pricing Page';
+
+    this.title.setTitle('Pricing Page');
+    this.meta.updateTag({
+      name: 'description',
+      content: 'Este en mi Pricing page',
+    });
+    this.meta.updateTag({ name: 'og:title', content: 'Pricing Page' });
+    this.meta.updateTag({ name: 'keywords', content: 'Hola,Loren,prueba' });
+  }
+}
