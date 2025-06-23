f678777f7f3349cf523cfee6b8b08e131bc2a895
LorenRodriguez <loren.rr@hotmail.com>
2025-06-23 13:41:16 +0200

    feat: enhance Pokémon routing and pagination with SSR support

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,9 +1,31 @@
+import { inject } from '@angular/core';
 import { RenderMode, ServerRoute } from '@angular/ssr';
+import { PokemonService } from './pokemons/services/pokemon.service';
+import { lastValueFrom } from 'rxjs';
+
+const TOTAL_POKEMONS = 10;
+
+const TOTAL_PAGES = 5;
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'pokemons/page/:page',
+    renderMode: RenderMode.Prerender,
+    async getPrerenderParams() {
+      return Array.from({ length: TOTAL_PAGES }, (_, i) => ({
+        page: (i + 1).toString(),
+      }));
+    },
+  },
+  {
     path: 'pokemons/:id',
-    renderMode: RenderMode.Server,
+    renderMode: RenderMode.Prerender,
+    async getPrerenderParams() {
+      const pokemonService = inject(PokemonService);
+
+      const pokemons = await lastValueFrom(pokemonService.loadPage(0));
+      return pokemons.map((pokemon) => ({ id: pokemon.name }));
+    },
   },
   {
     path: '**',
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,13 +2,12 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'pokemons',
+    path: 'pokemons/page/:page',
     loadComponent: () => import('./pages/pokemons/pokemons-page.component'),
   },
   {
     path: 'pokemons/:id',
-    loadComponent: () =>
-      import('./pages/pokemon/pokemon-page.component'),
+    loadComponent: () => import('./pages/pokemon/pokemon-page.component'),
   },
   {
     path: 'about',
